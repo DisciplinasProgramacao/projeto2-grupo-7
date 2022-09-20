bc2db1fc8e471dd0e4aabc97e8878adb8787f579
Rafaella Cristina <rafacssacrament@gmail.com>
2022-09-19 22:42:28 -0300

    criando novas classes para implementacao

---
+++ codigo/ArestaNew.java
@@ -0,0 +1,14 @@
+package codigo;
+
+public class ArestaNew {
+    private double peso;
+    private Vertice<TIPO> inicio;
+    private Vertice<TIPO> fim;
+
+    public ArestaNew(double peso, Vertice<TIPO> inicio, Vertice<TIPO> fim){
+        this.peso = peso;
+        this.inicio = inicio;
+        this.fim = fim;
+
+    }
+}
