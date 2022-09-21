a3325687d1294addd5269010dfac9b98faf68d8b
Vítor Lion <102547363+Lvion@users.noreply.github.com>
2022-09-21 20:49:13 -0300

    Update Grafo.java

--- codigo/Grafo.java
+++ codigo/Grafo.java
@@ -84,3 +84,34 @@ public class Grafo {
     }
 
 }
+
+
+public class GrafoCompleto extends Grafo{
+	
+	public GrafoCompleto(String nome) {
+	}
+
+	private int ordem;
+	
+	public boolean completo() {
+		return false;
+		
+	}
+	
+	public Aresta existeAresta(int vertice, int verticeB) {
+		return null;
+
+	}
+	
+	public Vertice existeVertice(int idVertice) {
+		return null;
+	}
+	
+	public boolean euleriano() {
+		return false;
+		
+	}
+	
+	public void subGrafo(ArrayList<Vertice>Vertices) {
+		
+	}
