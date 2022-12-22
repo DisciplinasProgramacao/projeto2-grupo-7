0304b3ca26cf26582411df8c5d1a86b35d489e28
Rafaella Cristina <rafacssacrament@gmail.com>
2022-12-22 00:07:15 -0300

    criando grafo completo

--- codigo/GrafoCompleto.java
+++ codigo/GrafoCompleto.java
@@ -7,6 +7,27 @@ public class GrafoCompleto extends Grafo {
         super(nome);
     }
 
+    public GrafoCompleto(String nome, int ordem) {
+        super(nome);
+        this.ordem = ordem;
+
+        Vertice[] arrVertices = new Vertice[this.ordem];
+
+        for (int i = 0; i < arrVertices.length; i++) {
+            arrVertices[i] = new Vertice(i);
+            for (int j = 0; j < arrVertices.length; j++) {
+                if (i == j)
+                    continue;
+                arrVertices[i].addAresta(j);
+            }
+        }
+
+        for (Vertice vertice : arrVertices) {
+            vertices.add(vertice.getId(), vertice);
+        }
+
+    }
+
     public boolean completo() {
         return false;
 
